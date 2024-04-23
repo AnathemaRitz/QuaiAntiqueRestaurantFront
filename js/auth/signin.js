d5dfc08d988dfaebb31f1dc77b12374414244afd
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-04-24 01:19:52 +0200

    simulation connexion

---
+++ js/auth/signin.js
@@ -0,0 +1,27 @@
+const mailInput= document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin =  document.getElementById("btnSignin");
+
+
+btnSignin.addEventListener("click", checkCredentials);
+    //informations factices, il faudra appeler l'API ici pour vérifier les credentials en BDD
+
+
+function checkCredentials(){
+    if(mailInput.value=="test@mail.com" && passwordInput.value =="123"){
+        alert("Vous êtes connecté");
+
+        //Il faudra récupérer le vrai token.
+        const token ="lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+
+        //placer ce token en cookie
+
+        
+        window.location.replace("/");
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid")
+
+    }
+}
